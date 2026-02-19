f1fbf29baa8f4506bd2100954be7dcbcb848e36d
maheshtce <80669949+maheshtce@users.noreply.github.com>
2026-02-18 19:54:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -34,18 +34,31 @@ col1.metric("Total Revenue Lift",
             f"${sim_df['revenue_delta'].sum():,.0f}")
 col2.metric("Avg Elasticity",
             f"{sim_df['elasticity'].mean():.2f}")
-col3.metric("Raise Candidates",
-            f"{(sim_df['elasticity'] > -1).sum()}")
+#col3.metric("Raise Candidates",
+#            f"{(sim_df['elasticity'] > -1).sum()}")
+
+col3.metric(
+    "Tier 1 Safe Raises",
+    f"{(sim_df['raise_tier'] == '🟢 Tier 1 – Safe Raise').sum():,}"
+)
 
 # Scatter Plot
-fig = px.scatter(
+#fig = px.scatter(
+#    sim_df,
+#    x="elasticity",
+#    y="avg_margin",
+#    color="segment",
+#    hover_data=["sku","region"],
+#    title="Elasticity vs Margin"
+#)
+fig_tier = px.histogram(
     sim_df,
-    x="elasticity",
-    y="avg_margin",
-    color="segment",
-    hover_data=["sku","region"],
-    title="Elasticity vs Margin"
+    x="raise_tier",
+    title="Raise Tier Distribution",
+    color="raise_tier"
 )
+st.plotly_chart(fig_tier, use_container_width=True)
+
 
 st.plotly_chart(fig, use_container_width=True)
 
